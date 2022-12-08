1aa106becf9dad1919c6110ace2947b5f7b3ba4f
Kartikey Kiroula <kartikeykiroula6789@gmail.com>
2022-12-09 01:46:08 +0530

    cm

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -7,7 +7,7 @@ const Signin = () => {
     const navigate = useNavigate()
     const [email, setEmail] = useState('');
     //const [walletaddress, setWalletaddress] = useState('');
-    const api = "http://52.66.196.48:3002"
+    const api = "https://cors-everywhere.herokuapp.com/http://52.66.196.48:3002"
 
     const handleChange = event => {
         setEmail(event.target.value);
