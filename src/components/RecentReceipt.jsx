3d8544175a42aa316d5877747a13a9284c891650
Kartikey Kiroula <kartikeykiroula6789@gmail.com>
2022-12-15 00:56:25 +0530

    Activity fix

--- src/components/RecentReceipt.jsx
+++ src/components/RecentReceipt.jsx
@@ -7,7 +7,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 const RecentReceipt = (props) => {
     const { walletaddress } = props
     console.log(walletaddress);
-    
+
     const [to1, setTo] = useState('');
     const [from1, setFrom] = useState('');
     const [amount1, setAmount] = useState('');
@@ -29,47 +29,50 @@ const RecentReceipt = (props) => {
             setExplorer('https://mumbai.polygonscan.com/tx/');
         }
 
-    const ADDRESS = walletaddress;
-    const apikey = "ZYDTV4HXTU8KRZ9EIQA263HK287Y514ZN8";
+        const ADDRESS = walletaddress;
+        const apikey = "ZYDTV4HXTU8KRZ9EIQA263HK287Y514ZN8";
 
-    try {
-      const etherscan = await axios.get(
-        endpoint +
-          `?module=account&action=txlist&address=${ADDRESS}&startblock=0
+        try {
+            const etherscan = await axios.get(
+                endpoint +
+                `?module=account&action=txlist&address=${ADDRESS}&startblock=0
             &endblock=99999999
             &page=1
             &offset=1000
             &sort=desc
             &apikey=${apikey}`
-      );
+            );
 
-      if (etherscan.data.message !== "OK")
-        throw new Error("Opps something went Wrong");
-      const arr = etherscan.data.result;
-      console.log(arr);
+            if (etherscan.data.message !== "OK")
+                throw new Error("Opps something went Wrong");
+            const arr = etherscan.data.result;
+            console.log(arr);
 
-      console.log(etherscan.data);
-      setRecentReceipts(arr.reverse());
-      console.log(explorer);
-      console.log(recentReceipts);
-      const result = recentReceipts[recentReceipts.length - 1];
-      setAmount(ethers.utils.formatUnits(result.value, "ether"));
-      setTo(result.to);
-      setFrom(result.from);
-      setLoading(false);
-      setError(null);
-      return result;
-    } catch (e) {
-      console.log(e.message);
-      // setError(e.message);
-      setLoading(false);
+            console.log(etherscan.data);
+            setRecentReceipts(arr.reverse());
+            console.log(explorer);
+            console.log(recentReceipts);
+            const result = recentReceipts[recentReceipts.length - 1];
+            setAmount(ethers.utils.formatUnits(result.value, "ether"));
+            setTo(result.to);
+            setFrom(result.from);
+            setLoading(false);
+            setError(null);
+            return result;
+        } catch (e) {
+            console.log(e.message);
+            // setError(e.message);
+            setLoading(false);
+        }
+    };
+    const handleChange = (e) => {
+        setChain(e.target.value);
+        txActivity();
     }
-  };
-  const handleChange = (e) => setChain(e.target.value);
 
-  React.useEffect(() => {
-    txActivity();
-  }, []);
+    React.useEffect(() => {
+        txActivity();
+    }, []);
 
     return <section className="w-full py-12 px-7 shadow-lg rounded-2xl w-fit mx-auto bg-[#141E2F] text-[20px]">
         <select value={chain} onChange={handleChange} className='p-4 rounded-full bg-[#263140] text-[#929292] w-full px-7'>
@@ -77,10 +80,10 @@ const RecentReceipt = (props) => {
             <option value="ethereum" className="py-4 text-[#929292]">Ethereum</option>
         </select>
         {
-            recentReceipts.length ?  recentReceipts.map((rec, i) => (
+            recentReceipts.length ? recentReceipts.map((rec, i) => (
                 <Receipt rec={rec} key={i} explorer={explorer} />
             )) : <div className="text-2xl text-center">No Transaction</div>
-           
+
         }
 
     </section>
@@ -98,6 +101,6 @@ const Receipt = ({ rec, explorer }) => {
             <a href={explorer + rec.hash} className="text-[#17987F] font-semibold">View Transaction</a>
         </div>
     </div>
-};                        
+};
 
 export default RecentReceipt;
