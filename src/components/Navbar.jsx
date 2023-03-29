db92820c3c1e8ac6d6187a33eafdb9e3e766543c
Rupali Haldiya <rupali7487@gmail.com>
2023-03-29 18:37:24 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import logo from "../assets/custodial-wallet.png";
 
 const Navbar = () => {
-    return <div className="container flex justify-between mx-auto py-10">
-                <h2 className="text-2xl text-white font-bold">Logo</h2>
+  return (
+    <div className="container mx-auto">
+      <img src={logo} alt="custodial wallet logo" className="w-52" />
+    </div>
+  );
+};
 
-                {/* <h2 className="text-white px-4 py-2 border rounded-full text-center text-xl bg-transparent">
-                BRBk....sLJm
-                </h2> */}
-           </div>
-}
-
-export default Navbar
+export default Navbar;
