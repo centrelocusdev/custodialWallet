2c7b7115477650dc870f2c34c90d94bd4b9fd342
Kartikey Kiroula <kartikeykiroula6789@gmail.com>
2024-08-22 16:22:36 +0530

    Update ERC20.jsx

--- src/components/ERC20.jsx
+++ src/components/ERC20.jsx
@@ -83,10 +83,10 @@ const ERC20 = (props) => {
         className="p-4 rounded-full bg-[#263140] text-[#929292] w-full px-7"
       >
         <option value="mumbai" className="py-4 text-[#929292]">
-          Mumbai
+          Polygon Amoy
         </option>
         <option value="ethereum" className="py-4 text-[#929292]">
-          Ethereum
+          Ethereum Sepolia
         </option>
       </select>
 
